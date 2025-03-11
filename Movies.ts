5ab14f9a5a497bc06894c1fdef381def93b020a9
Divya Rani <divyaranidhkls@gmail.com>
2025-03-12 04:55:14 +0530

    Add getTopRated

--- Movies.ts
+++ Movies.ts
@@ -1,58 +1,55 @@
 import { constants } from "node:fs/promises";
 
-  type Movie = 
-  {
+type Movie = {
+  id: string;
+  title: string;
+  director: string;
+  release_Year: Date;
+  genre: string;
+  rating: number[];
+};
 
-    id:string;
-    title:string;
-    director:string;
-    release_Year:Date;
-    genre:string;
-    rating:number[];
-
-  };
-  
-  export class MovieManagement
-  {
-      private movies:Map<string,Movie>;
-    constructor()
-    {
-        this.movies=new Map<string,Movie>();
-    }
-
-  addMovie(id:string,title:string,director:string,release_Year:Date,genre:string) : string
-    {
-        const newMovie={id,title,director,release_Year,genre,rating:[]};
-        this.movies.set(id,newMovie);
-        return "Movie Added Succefully";
-    }
-
-   rateMovie(id:string,rating:number) : Boolean
-   {
-        const movies=this.movies.get(id);
-        if(movies && (rating>=1 && rating<=5))
-        {
-            movies.rating.push(rating);
-            return true;
-        }
-        return false;
-   }
-
- getAverageRating(id:string) : number
- {
-
-    const movie=this.movies.get(id);
-    if(movie)
-    {
-        const sum=movie.rating.reduce((acc,curr)=> acc + curr,0);
-        return sum/movie.rating.length;
-    }
- return -1;
-
-
-
- }
-
-
-
+export class MovieManagement {
+  private movies: Map<string, Movie>;
+  constructor() {
+    this.movies = new Map<string, Movie>();
   }
+
+  addMovie(
+    id: string,
+    title: string,
+    director: string,
+    release_Year: Date,
+    genre: string
+  ): string {
+    const newMovie = { id, title, director, release_Year, genre, rating: [] };
+    this.movies.set(id, newMovie);
+    return "Movie Added Succefully";
+  }
+
+  rateMovie(id: string, rating: number): Boolean {
+    const movies = this.movies.get(id);
+    if (movies && rating >= 1 && rating <= 5) {
+      movies.rating.push(rating);
+      return true;
+    }
+    return false;
+  }
+
+  getAverageRating(id: string): number {
+    const movie = this.movies.get(id);
+    if (movie) {
+      const sum = movie.rating.reduce((acc, curr) => acc + curr, 0);
+      return sum / movie.rating.length;
+    }
+    return -1;
+  }
+  getTopRatedMovies() {
+    const TopRated = Array.from(this.movies.values()).sort((a, b) => {
+      const AvgA = this.getAverageRating(a.id);
+      const AvgB = this.getAverageRating(b.id);
+
+      return AvgA - AvgB;
+    });
+  }
+}
--- index.ts
+++ index.ts
@@ -1,9 +1,26 @@
 import { MovieManagement } from "./Movies";
-const Movie=new MovieManagement();
-console.log(Movie.addMovie("123","Kantara","Rishab",new Date("2021-04-04"),"Devotional"));
-console.log(Movie.addMovie("125","Super","Upendra",new Date("2019-04-04"),"Intelligent"));
-console.log(Movie.rateMovie("123",4));
-console.log(Movie.rateMovie("123",4));
-console.log(Movie.rateMovie("123",5));
-console.log(Movie.rateMovie("123",3));
-console.log(Movie.getAverageRating);
+const Movie = new MovieManagement();
+console.log(
+  Movie.addMovie(
+    "123",
+    "Kantara",
+    "Rishab",
+    new Date("2021-04-04"),
+    "Devotional"
+  )
+);
+console.log(
+  Movie.addMovie(
+    "125",
+    "Super",
+    "Upendra",
+    new Date("2019-04-04"),
+    "Intelligent"
+  )
+);
+console.log(Movie.rateMovie("123", 4));
+console.log(Movie.rateMovie("123", 4));
+console.log(Movie.rateMovie("123", 5));
+console.log(Movie.rateMovie("123", 3));
+console.log(Movie.getAverageRating("123"));
+console.log(Movie.getTopRatedMovies);
